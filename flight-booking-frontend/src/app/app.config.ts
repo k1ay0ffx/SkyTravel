7dfff6e9ce6947852b4dcff1012c45bfb939785e
klay <xclay315@gmail.com>
2026-04-20 20:34:52 +0500

    before angular update

--- flight-booking-frontend/src/app/app.config.ts
+++ flight-booking-frontend/src/app/app.config.ts
@@ -1,12 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideHttpClient()
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient()
   ]
 };
